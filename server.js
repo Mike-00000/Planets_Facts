0f2efafa6a790a1d04abcdc4a6f823d8e42a0495
Mike <mt.mail.mt@gmail.com>
2023-10-22 21:21:25 +0300

    Commiting V1

--- server.js
+++ server.js
@@ -47,7 +47,6 @@ app.get('/api/planets/:planetName', async (req, res) => {
     }
 });
 
-
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
